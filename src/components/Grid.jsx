f79caad0c920d7ed172045bec38dd76f6c449580
iwikjg <kjg@iwi.co.kr>
2022-12-06 17:34:12 +0900

    그리드 작업중

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import imgDownArrow from "../resources/images/down-arrow.svg";
 import imgDownArrowDisabled from "../resources/images/down-arrow-disabled.svg";
+import discard from "../resources/images/restart.svg";
+
+// #fefbf5
 
 const Wrap = styled.div`
   border: 1px solid #ccc;
@@ -19,117 +22,94 @@ const Wrap = styled.div`
   &:hover::-webkit-scrollbar-thumb {
     background-color: #999;
   }
+
+  & * {
+    box-sizing: border-box;
+    color: #000;
+  }
 `;
 
-const Box = styled.div`
-  display: grid;
-  grid-gap: 0;
-`;
+const Table = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0;
 
-const Cell = styled.div`
-  background-color: #fff;
-  border-right: solid 1px #ccc;
-  border-bottom: solid 1px #ccc;
-  box-shadow: 0 0 0 2px inset transparent;
-  transition: all 0.1s ease-in-out;
-  cursor: default;
-
-  &[data-type="header"] {
+  /* header */
+  & thead th,
+  & thead td {
     position: sticky;
     top: 0;
-    background-color: #f3f3f3;
-    font-weight: bold;
-    padding: 0 10px;
-  }
-  &[data-type="footer"] {
-    position: sticky;
-    bottom: 0;
-    background-color: #f3f3f3;
-    font-weight: bold;
-    text-align: right;
-    border-top: solid 1px #ccc;
-    border-bottom: none;
-    border-right: none;
-    grid-column: 1/-1;
-    padding: 0 10px;
-  }
-  &[data-type="nodata"] {
-    border-bottom: none;
-    border-right: none;
-    grid-column: 1/-1;
-    text-align: center;
-    &:before {
-      content: "no data";
-    }
-  }
-  &.fr {
-    border-top: none;
-  }
-  &.lr {
-    border-bottom: none;
-  }
-  &.lc {
-    border-right: none;
-  }
-  &.rowSelect {
-    background-color: rgba(0, 0, 0, 0.05);
-    font-weight: bold;
-  }
-  &.colSelect {
-    box-shadow: 0 0 0 1px inset rgba(0, 0, 0, 0.5);
+    z-index: 1;
+    background: #f3f3f3;
   }
 
-  & > * {
-    display: block;
-    width: 100%;
-    height: 100%;
-    padding: 0 10px;
+  & > *:last-child > tr:last-child > th,
+  & > *:last-child > tr:last-child > td {
+    border-bottom: none;
+  }
+  & > * > tr > th:last-child,
+  & > * > tr > td:last-child {
+    border-right: none;
   }
 
-  & input {
-    border: none;
-    width: 100%;
-    height: 100%;
-    padding: 0 10px;
-    color: inherit;
-    opacity: 1;
-    background-color: #fefbf5;
-    &:focus {
-      position: relative;
-      z-index: 1;
-    }
-    &:disabled {
-      background-color: transparent;
-      &.editable {
-        background-color: #fefbf5;
+  /* cell */
+  & th,
+  & td {
+    border-right: solid 1px #ccc;
+    border-bottom: solid 1px #ccc;
+    padding: 0;
+    cursor: default;
+    position: relative;
+
+    /* data */
+    & > * {
+      display: block;
+      width: 100%;
+      height: 100%;
+      padding: 0 10px;
+      border: none;
+      &:focus {
+        position: relative;
+        outline-color: #1e90ff;
+        outline-style: solid;
+        outline-width: 2px;
+        outline-offset: -2px;
       }
     }
-  }
-
-  & select {
-    border: none;
-    width: 100%;
-    height: 100%;
-    padding: 0 30px 0 10px;
-    appearance: none;
-    color: inherit;
-    opacity: 1;
-    background-image: url(${imgDownArrow});
-    background-repeat: no-repeat;
-    background-size: 8px;
-    background-position: calc(100% - 10px) 50%;
-    background-color: #fefbf5;
-    &:focus {
-      position: relative;
-      z-index: 1;
-    }
-    &:disabled {
-      background-image: none;
+    & > select {
+      appearance: none;
+      /* background-color: #fefbf5; */
       background-color: transparent;
-      &.editable {
-        background-image: url(${imgDownArrowDisabled});
-        background-color: #fefbf5;
+      background-image: url(${imgDownArrow});
+      background-repeat: no-repeat;
+      background-position: calc(100% - 10px) 50%;
+      background-size: 8px;
+      opacity: 1;
+      &:disabled {
+        /* background-color: transparent; */
+        background-image: none;
       }
+    }
+    & > input {
+      /* background-color: #fefbf5; */
+      background-color: transparent;
+    }
+    /* & em {
+      display: none;
+      position: absolute;
+      width: 16px;
+      height: 16px;
+      right: 8px;
+      top: calc(50% - 8px);
+      background-image: url(${discard});
+      background-repeat: no-repeat;
+      cursor: pointer;
+    } */
+    &[data-modified="true"] {
+      background-color: #fffdd6;
+      /* & em {
+        display: block;
+      } */
     }
   }
 `;
@@ -138,41 +118,76 @@ export const Grid = ({ id, columns, data, config }) => {
   const [row, setRow] = useState(-1);
   const [col, setCol] = useState(-1);
 
+  const [focusedCell, setFocusedCell] = useState({});
+
   useEffect(() => {
-    if (row >= 0 && col >= 0) {
-      document.querySelectorAll("[data-type='column']").forEach((cell) => {
-        if (cell.dataset.row === row) {
-          cell.classList.add("rowSelect");
-          if (cell.dataset.col === col) {
-            cell.classList.add("colSelect");
-          } else {
-            cell.classList.remove("colSelect");
-          }
-        } else {
-          cell.classList.remove("rowSelect");
-          cell.classList.remove("colSelect");
-        }
-      });
-    }
+    console.log("row/col", row, col);
   }, [row, col]);
 
   if (!id || !columns) {
     return null;
   }
 
-  const rowClass = (rowIndex) => {
+  const handleEditableBlur = ({ target }) => {
+    if (document.selection) {
+      document.selection.empty();
+    } else if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+    }
+
+    target.disabled = true;
+    target.removeEventListener("blur", handleEditableBlur);
+  };
+
+  const getCell = (target) => {
+    let cell = target;
+    let scope = cell?.dataset?.scope;
+    if (scope !== "cell") {
+      while (scope !== "cell") {
+        if (!cell.parentElement) break;
+        cell = cell.parentElement;
+        scope = cell.dataset?.scope;
+      }
+    }
+    return cell;
+  };
+
+  const onCellDoubleClick = ({ target }) => {
+    const cell = getCell(target);
     try {
-      return rowIndex === 0 ? "fr" : rowIndex + 1 === data.length ? "lr" : "";
+      const editable = JSON.parse(cell.dataset.editable);
+      if (editable) {
+        const input = cell.querySelector("input,select");
+        input.disabled = false;
+        input.focus();
+        if (input.nodeName === "INPUT") input.select();
+        input.addEventListener("blur", handleEditableBlur);
+      }
     } catch (e) {
-      return "";
+      // do nothing
     }
   };
 
-  const columnClass = (colIndex) => {
+  const onCellChange = ({ target }) => {
+    const cell = getCell(target);
+    cell.dataset.value = target.value;
+    cell.dataset.modified = cell.dataset.value !== cell.dataset.origin;
+  };
+
+  const onCellFocus = ({ target }) => {
+    const cell = getCell(target);
     try {
-      return colIndex === 0 ? "fc" : colIndex + 1 === columns.length ? "lc" : "";
+      setFocusedCell({ target: cell, ...cell.dataset });
     } catch (e) {
-      return "";
+      // do anything
+    }
+  };
+
+  const onCellBlur = ({ target }) => {
+    try {
+      setFocusedCell({});
+    } catch (e) {
+      // do anything
     }
   };
 
@@ -181,102 +196,82 @@ export const Grid = ({ id, columns, data, config }) => {
     setCol(target.getAttribute("data-col"));
   };
 
-  const onCellDblClick = ({ target }) => {
-    let column = target;
-    while (column.dataset.type !== "column") {
-      column = column.parentElement;
-      if (!column) break;
-    }
-    if (column.dataset.editable && column.dataset.editable === "true") {
-      target.removeAttribute("disabled");
-      target.focus();
-      if (target.nodeName === "INPUT") target.select();
-      target.onblur = () => {
-        target.setAttribute("disabled", true);
-      };
-    }
-  };
-
-  const onCellChange = (rowIndex, colIndex, target) => {
-    setCellValue(rowIndex, colIndex, target.value);
-  };
-
-  const setCellValue = (rowIndex, colIndex, value) => {
-    try {
-      const cell = document.querySelector(
-        `[data-type='column'][data-row='${rowIndex}'][data-col='${colIndex}'][data-editable='true']`
-      );
-      cell.dataset.val = value;
-      cell.querySelector("select,input,data").value = value;
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   const wrapStyle = {};
+  const cellStyle = {};
   if (config) {
     if (config.gridHeight) wrapStyle.maxHeight = config.gridHeight;
-    if (config.rowHeight) wrapStyle.lineHeight = config.rowHeight;
+    if (config.rowHeight) {
+      cellStyle.lineHeight = config.rowHeight;
+      cellStyle.height = config.rowHeight;
+    }
   }
 
   return (
     <Wrap id={id} style={{ ...wrapStyle }}>
-      <Box style={{ gridTemplateColumns: `repeat(${columns.length}, auto)` }}>
-        {columns.map((column, i) => (
-          <Cell key={i} data-type='header' className={`${columnClass(i)} `}>
-            {column.name}
-          </Cell>
-        ))}
-        {(!data || data.length === 0) && <Cell data-type='nodata'></Cell>}
-        {data &&
-          data.map((row, rowIndex) =>
-            columns.map((column, colIndex) => {
-              const { id, name, editable, type, data } = column;
-              const value = row[id];
-              return (
-                <Cell
-                  key={`${rowIndex}_${colIndex}`}
-                  className={`${rowClass(rowIndex)} ${columnClass(colIndex)}`}
-                  onClick={onCellClick}
-                  onDoubleClick={onCellDblClick}
-                  data-type='column'
-                  data-row={rowIndex}
-                  data-col={colIndex}
-                  data-editable={editable}
-                  data-val={value}
-                  data-org={value}
-                >
-                  {type === "combo" && data && data.length > 0 ? (
-                    <select
-                      defaultValue={value}
-                      disabled={true}
-                      className={editable ? "editable" : null}
-                      onChange={({ target }) => onCellChange(rowIndex, colIndex, target)}
-                    >
-                      <option value=''></option>
-                      {data.map(({ value: v, label: l }) => (
-                        <option key={v} value={v}>
-                          {l}
-                        </option>
-                      ))}
-                    </select>
-                  ) : editable ? (
-                    <input
-                      type='text'
-                      defaultValue={value}
-                      disabled={true}
-                      className={editable ? "editable" : null}
-                      onChange={({ target }) => onCellChange(rowIndex, colIndex, target)}
-                    />
-                  ) : (
-                    <data value={value}>{value}</data>
-                  )}
-                </Cell>
-              );
-            })
-          )}
-        <Cell data-type='footer'>Footer</Cell>
-      </Box>
+      <Table>
+        <thead>
+          <tr>
+            {columns.map((column, i) => (
+              <th key={i} style={{ ...cellStyle }} data-cid={column.id} data-scope='header'>
+                {column.name}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((row, ridx) => (
+            <tr key={ridx} data-ridx={ridx} data-rid={ridx} data-scope='row'>
+              {columns.map((column, cidx) => {
+                const cellValue = row[column.id];
+                const editable = column.editable ?? false;
+                const cellType = column.type;
+                const cellData = column.data;
+                return (
+                  <td
+                    key={cidx}
+                    style={{ ...cellStyle }}
+                    data-modified={false}
+                    data-ridx={ridx}
+                    data-rid={ridx}
+                    data-cidx={cidx}
+                    data-cid={column.id}
+                    data-scope='cell'
+                    data-value={cellValue ? cellValue : ""}
+                    data-origin={cellValue ? cellValue : ""}
+                    data-editable={editable ? true : false}
+                    data-type={cellType ? cellType : ""}
+                    onDoubleClick={onCellDoubleClick}
+                    onFocus={onCellFocus}
+                    onBlur={onCellBlur}
+                  >
+                    {cellType === "combo" && cellData && cellData.length > 0 ? (
+                      <select defaultValue={cellValue} disabled={true} onChange={onCellChange}>
+                        <option></option>
+                        {cellData.map((list) => (
+                          <option key={list.value} value={list.value}>
+                            {list.label}
+                          </option>
+                        ))}
+                      </select>
+                    ) : editable ? (
+                      <input type='text' defaultValue={cellValue} disabled={true} onChange={onCellChange} />
+                    ) : (
+                      <data value={cellValue}>{cellValue}</data>
+                    )}
+
+                    {/* <em></em> */}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+        {/* <tfoot>
+          <tr>
+            <th data-cid={column.id} data-scope='footer'>footer</th>
+          </tr>
+        </tfoot> */}
+      </Table>
     </Wrap>
   );
 };
